0b7d8d8a72fafccccfb90db4f2586d537e548ab4
norhanahmedsaber <norhan3182001@gmail.com>
2024-10-27 01:33:27 +0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors';
+
 import userRoutes from './routes/userRoutes';
 import organizationRoutes from './routes/organizationRoutes';
 
@@ -8,7 +10,13 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
-
+app.use(
+    cors({
+      origin: 'http://localhost:5173', 
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+      credentials: true 
+    })
+  );
 app.use(userRoutes);
 app.use(organizationRoutes);
 
